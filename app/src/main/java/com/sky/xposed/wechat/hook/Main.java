931018473ce83f8ae114118435c42907d93da2d0
sky <jingcai.wei@163.com>
2018-03-10 18:36:16 +0800

    初始化项目

---
+++ app/src/main/java/com/sky/xposed/wechat/hook/Main.java
@@ -0,0 +1,16 @@
+package com.sky.xposed.wechat.hook;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by sky on 18-3-10.
+ */
+
+public class Main implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+    }
+}
